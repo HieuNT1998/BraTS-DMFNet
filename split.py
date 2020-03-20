fac5655ca14d9afbe519d54508fca4af9a3c5850
HieuNT1998 <trihieua2@gmail.com>
2020-03-20 13:47:21 +0700

    update split.py

--- split.py
+++ split.py
@@ -7,8 +7,8 @@ import os
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
 
-root = './Training_folder'
-valid_data_dir = './Validation_folder'
+root = '../data/2018/MICCAI_BraTS_2018_Data_Training'
+valid_data_dir = '../data/2018/MICCAI_BraTS_2018_Data_Validation'
 
 
 def write(data, fname, root=root):
